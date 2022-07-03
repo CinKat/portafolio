aa6a1158067b5b297a4e751725d395a62cf82e1e
katerin <ckaterinarellano@gmail.com>
2022-07-03 10:18:53 -0500

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Navbar from "./components/Navbar";
 import React from 'react';
-import Hero from "./components/Hero";
+import Inicio from "../src/pages/Inicio";
+import Acerca from "../src/pages/Acerca"
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Hero />
-    </div>
+      <main>
+        <Routes>
+          <Route index element={<Navigate to="/inicio" />} />
+          <Route path="*" element={<Navigate to="/inicio" />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/acerca" element={<Acerca />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { Global } from "@emotion/react";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { global, reset } from "./styles";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Global styles={reset} />
-    <Global styles={global} />
-    <App />
+    <BrowserRouter>
+      <Global styles={reset} />
+      <Global styles={global} />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
